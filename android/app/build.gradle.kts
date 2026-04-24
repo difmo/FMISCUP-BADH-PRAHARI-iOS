c00eea8711cad7a935a95019bb23d766b721f592
difmo <difmotech@gmail.com>
2026-04-24 11:46:40 +0530

    save

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.techwings.fmiscupapp2"
-    compileSdk = 35
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
